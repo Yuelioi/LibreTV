961480ddb698cf3ed55029f29838fe3c71f66e40
yueli <yuelioi1210@gmail.com>
2025-07-12 01:12:08 +0800

    fix

--- js/hm.js
+++
@@ -1,9 +0,0 @@
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?2634de5cb67b82d7e5db5bd0f65eaad3";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
