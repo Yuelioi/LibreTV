a632b1313203216325b0f26fd4eebc7f60caf119
yueli <yuelioi1210@gmail.com>
2025-07-12 01:03:53 +0800

    add baidu tongji

---
+++ js/tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2634de5cb67b82d7e5db5bd0f65eaad3";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
